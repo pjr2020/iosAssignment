b349971f2722661c92d584e0b58915db99ea7050
imani-abayakoon <imani@apium.io>
2022-05-09 21:08:27 +1000

    Test commit

--- News/Controller/ArticleViewController.swift
+++ News/Controller/ArticleViewController.swift
@@ -13,7 +13,10 @@ class ArticleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("test")
     }
+    
+    
 
 
 }
