bd3597a29d14c8a872583657370bec8cc559fe16
Finn <peijianrui@126.com>
2022-05-10 12:58:08 +1000

    framework

--- News/Controller/HomeViewController.swift
+++ News/Controller/HomeViewController.swift
@@ -8,12 +8,40 @@
 import Foundation
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    var articles = [1,2,3]
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return articles.count
     }
 
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+    let cell =  tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as! ArticleTableViewCell
+    let index = articles[indexPath.row]
+    //cell.ArticleImage?.image = UIImage(systemName: "Homekit")
+        cell.test?.text = "\(index)123"
+    return cell
+
+    }
 }
+
+class ArticleTableViewCell:UITableViewCell{
+    
+    @IBOutlet weak var test: UILabel!
+    
+}
+
+
--- News/Controller/SettingViewController.swift
+++ News/Controller/SettingViewController.swift
@@ -14,6 +14,6 @@ class SettingViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
